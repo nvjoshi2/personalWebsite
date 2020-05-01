6030418041c18bd3fc3a00dd17a79be91e318811
Nathan Joshi <nvj1300@gmail.com>
2020-04-30 20:08:49 -0500

    added another portfolio item

--- src/components/pages/PortfolioPage.js
+++ src/components/pages/PortfolioPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TimeItPic from '../../timeit_screenshot.png';
+import LatexPic from '../../latex_screenshot.png'
 import '../PortfolioPage.css';
 const PortfolioPage = (props) => {
 
@@ -11,6 +12,15 @@ const PortfolioPage = (props) => {
                     <h3 className = 'title'>TimeIt</h3>
                     <div className = 'visit-button'>
                         <a href="http://timeit.me.s3-website-us-east-1.amazonaws.com/" target="_blank">Visit</a>
+                    </div>
+                </div>
+            </div>
+            <div className = 'portfolio-item'>
+                <img className = 'website-image' src={LatexPic}/>
+                <div className = 'overlay'>
+                    <h3 className = 'title'>Text to LaTeX</h3>
+                    <div className = 'visit-button'>
+                        <a href="https://www.youtube.com/watch?v=dUh9TwvH2ZM" target="_blank">View Demo</a>
                     </div>
                 </div>
             </div>
