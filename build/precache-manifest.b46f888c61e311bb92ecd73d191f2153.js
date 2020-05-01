c56a95d64973a159f61fe64e57842b5c372a35b9
Nathan Joshi <nvj1300@gmail.com>
2020-04-30 20:36:09 -0500

    added _redirects

---
+++ build/precache-manifest.b46f888c61e311bb92ecd73d191f2153.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f98c9da1cb857203534e7e00e6e474e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2c8b3ff4dc206175126",
+    "url": "/static/css/main.8082cd5c.chunk.css"
+  },
+  {
+    "revision": "35643d46ceba2df208fe",
+    "url": "/static/js/2.a1fb4360.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a1fb4360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c8b3ff4dc206175126",
+    "url": "/static/js/main.d442fa7f.chunk.js"
+  },
+  {
+    "revision": "863c4928fe8dd23410a8",
+    "url": "/static/js/runtime-main.71afa878.js"
+  },
+  {
+    "revision": "746f1a302ae0ad1cbda81eaa1a914e5b",
+    "url": "/static/media/japan_pic.746f1a30.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a340f4779d891e34df10d09a8ee94e0f",
+    "url": "/static/media/my_resume.a340f477.pdf"
+  }
+]);
